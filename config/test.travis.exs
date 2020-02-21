16058b2e033259b2e6c27f8c285133a014f65dda
Trey Evans <tevans@Just-Plain-Awful.local>
2020-02-21 18:03:48 -0500

    Travis.

---
+++ config/test.travis.exs
@@ -0,0 +1,21 @@
+use Mix.Config
+
+# Configure your database
+config :whats_where, WhatsWhere.Repo,
+  database: "whats_where_test",
+  hostname: "localhost",
+  username: "postgres",
+  password: "",
+  pool: Ecto.Adapters.SQL.Sandbox
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :whats_where, WhatsWhereWeb.Endpoint,
+  http: [port: 4002],
+  server: false
+
+config :junit_formatter,
+  report_dir: "."
+
+# Print only warnings and errors during test
+config :logger, level: :warn
